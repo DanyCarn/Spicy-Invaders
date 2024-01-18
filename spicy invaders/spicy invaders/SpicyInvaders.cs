518fc0dd491f92225ef30a873224bd9df1967733
Dany Carneiro <pc23wsc@eduvaud.ch>
2024-01-18 14:17:40 +0100

    first commit

---
+++ spicy invaders/spicy invaders/SpicyInvaders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spicy_invaders
+{
+    internal class SpicyInvaders
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
